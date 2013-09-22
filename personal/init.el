100ab860f9228d0baef8091a3ef38657bdf783cf
Jesús Barbero Rodríguez <chuchiperriman@gmail.com>
2013-09-23 01:39:30 +0200

    jedi, multi-web y pequeños retoques

--- personal/init.el
+++ personal/init.el
@@ -1,4 +1,5 @@
-(prelude-require-packages '(emmet-mode))
+;; jedi necesita el paquete (python) erp y jedi instalados
+(prelude-require-packages '(emmet-mode jedi multi-web-mode))
 
 (setq prelude-flyspell nil)
 (setq prelude-guru nil)
@@ -22,4 +23,26 @@
 (add-hook 'css-mode-hook  'emmet-mode)
 (define-key global-map (kbd "C-e") 'emmet-expand-line)
 
+;;Quitamos smartparents para poder seleccionar usando C-right
 (eval-after-load "prelude-editor" '(smartparens-global-mode -1))
+
+;; Borrar línea con control + d
+(global-set-key (kbd "C-d") 'prelude-kill-whole-line)
+
+;;jedi, autocompletado python
+;;(add-hook 'python-mode-hook 'auto-complete-mode)
+(add-hook 'python-mode-hook 'jedi:setup)
+(defvar jedi:setup-keys t)
+(defvar jedi:complete-on-dot t)
+
+(add-hook 'after-init-hook #'global-flycheck-mode)
+
+;;Soporte para javascript dentro de html
+(require 'multi-web-mode)
+(setq mweb-default-major-mode 'html-mode)
+(setq mweb-tags '((php-mode "<\\?php\\|<\\? \\|<\\?=" "\\?>")
+                  (js-mode "<script[^>]*>" "</script>")
+                  (css-mode "<style +type=\"text/css\"[^>]*>" "</style>")))
+(setq mweb-filename-extensions '("php" "htm" "html" "ctp" "phtml" "php4" "php5"))
+(multi-web-global-mode 1)
+
